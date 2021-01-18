1be18724c147dddfff0908e73ed44adf3d5daa8a
Austin Zheng <austin.zheng@ringcentral.com>
2021-01-18 08:58:50 +0800

    Initial Commit

---
+++ WeChat_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeChat_Demo
+//
+//  Created by Austin Zheng on 2021/1/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
